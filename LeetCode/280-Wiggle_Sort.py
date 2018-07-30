dfc6c7d919c82a51affbb5da0368e8a7a5919ad9
eren <eren@blackberry.com>
2018-07-30 08:53:11 -0400

    Wiggle Sort O(n)

--- LeetCode/280-Wiggle_Sort.py
+++ LeetCode/280-Wiggle_Sort.py
@@ -1,24 +1,22 @@
-import math
 class Solution(object):
+
     def wiggleSort(self, nums):
-        nums_s = sorted(nums)
-        first_half = nums_s[:int(math.ceil(len(nums_s)/2))]
-        second_half = nums_s[int(math.ceil(len(nums_s)/2)):]
-        print(first_half, second_half)
-        output = []
-        while second_half and first_half:
-            output.append(first_half.pop(0))
-            output.append(second_half.pop(0))
-        if first_half:
-            output.append(first_half.pop(0))
-        if second_half:
-            output.append(second_half.pop(0))
-        nums[:] = list(output)
-        print(nums[:])
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                if nums[i] > nums[i-1]:
+                    temp = nums[i]
+                    nums[i] = nums[i-1]
+                    nums[i-1] = temp
+            else:
+                if nums[i] < nums[i-1]:
+                    temp = nums[i]
+                    nums[i] = nums[i-1]
+                    nums[i-1] = temp
+        nums[:] = nums
 
 
 
 a = Solution()
-b = [1,2,3]
+b = [1, 2, 3]
 print(a.wiggleSort(b))
 print(b)
