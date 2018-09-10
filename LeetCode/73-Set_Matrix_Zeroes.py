0c26068eb84d26045823aea4d9a457c5986c4819
Eddie <eddie.ren.2013@gmail.com>
2018-09-10 00:38:07 -0400

    Set matrix zeroes: Concise solution

--- LeetCode/73-Set_Matrix_Zeroes.py
+++ LeetCode/73-Set_Matrix_Zeroes.py
@@ -1,48 +1,68 @@
 class Solution:
     def setZeroes(self, matrix):
-        ver, hor = False, False
-        for i in matrix:
-            if i[0] == 0:
-                ver = True
+        col_zero = 1
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        for i in matrix[0]:
-            if i == 0:
-                hor = True
-
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[i])):
-                if matrix[i][j] == 0:
-                    matrix[0][j] = 0
-                    matrix[i][0] = 0
-        # print("set outer zeroes", matrix)
-
-        for i in range(1, len(matrix)):
+        for i in range(rows):
             if matrix[i][0] == 0:
-                for j in range(len(matrix[i])):
+                col_zero = 0
+            for j in range(1, cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(rows-1, -1, -1):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-        # print("checked vertical zeroes", matrix)
-
-        for i in range(1, len(matrix[0])):
-            if matrix[0][i] == 0:
-                for j in range(len(matrix)):
-                    matrix[j][i] = 0
-        # print("checked horizontal zeroes", matrix)
-
-        if ver:
-            for i in range(len(matrix)):
+            if col_zero == 0:
                 matrix[i][0] = 0
-        if hor:
-            for i in range(len(matrix[0])):
-                matrix[0][i] = 0
-
         matrix[:] = matrix
+
+        # ver, hor = False, False
+        # for i in matrix:
+        #     if i[0] == 0:
+        #         ver = True
+        #
+        # for i in matrix[0]:
+        #     if i == 0:
+        #         hor = True
+        #
+        # for i in range(1, len(matrix)):
+        #     for j in range(1, len(matrix[i])):
+        #         if matrix[i][j] == 0:
+        #             matrix[0][j] = 0
+        #             matrix[i][0] = 0
+        # # print("set outer zeroes", matrix)
+        #
+        # for i in range(1, len(matrix)):
+        #     if matrix[i][0] == 0:
+        #         for j in range(len(matrix[i])):
+        #             matrix[i][j] = 0
+        # # print("checked vertical zeroes", matrix)
+        #
+        # for i in range(1, len(matrix[0])):
+        #     if matrix[0][i] == 0:
+        #         for j in range(len(matrix)):
+        #             matrix[j][i] = 0
+        # # print("checked horizontal zeroes", matrix)
+        #
+        # if ver:
+        #     for i in range(len(matrix)):
+        #         matrix[i][0] = 0
+        # if hor:
+        #     for i in range(len(matrix[0])):
+        #         matrix[0][i] = 0
+        #
+        # matrix[:] = matrix
         # return matrix
 
 a = Solution()
 b = [
-  [1,0,1, 1],
-  [1,1,1,1],
-  [1,1,0,1]
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
 ]
 print(a.setZeroes(b))
 print(b)
