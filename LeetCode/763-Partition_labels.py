2d628d1a3195a946ebbae59e0cb88e5a02ff8429
Eddie <eddie.ren.2013@gmail.com>
2018-09-11 15:31:10 -0400

    Partition labels

---
+++ LeetCode/763-Partition_labels.py
@@ -0,0 +1,19 @@
+import collections
+
+class Solution():
+    def partitionLabels(self, nums):
+        dict = {c:i for i, c in enumerate(nums)}
+        cur = 0
+        start = 0
+        res = []
+        for i, j in enumerate(nums):
+            cur = max(cur, dict[j])
+            if i == cur:
+                print("indexed", nums[start:cur])
+                res.append(1+cur-start)
+                start = cur+1
+        return res
+
+a = Solution()
+b = "ababcbacadefegdehijhklij"
+print(a.partitionLabels(b))
