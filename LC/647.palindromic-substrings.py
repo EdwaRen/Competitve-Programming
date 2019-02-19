4c3efc65c241e0343bf20619051cc4c0cacb5753
Eddie <eddie.ren.2013@gmail.com>
2019-02-19 04:51:58 -0700

    canJump

---
+++ LC/647.palindromic-substrings.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+def countSubstrings(S):
+    def manachers(S):
+        A = '@#' + '#'.join(S) + '#$'
+        print('#'.join(S))
+        Z = [0] * len(A)
+        center = right = 0
+        print("entering loop")
+        for i in xrange(1, len(A) - 1):
+            print("iter ", i, Z[i], right)
+            if i < right:
+                print("right activated")
+                Z[i] = min(right - i, Z[2 * center - i])
+            print(Z[i])
+            while A[i + Z[i] + 1] == A[i - Z[i] - 1]:
+                Z[i] += 1
+            print(Z[i], A[i])
+            if i + Z[i] > right:
+                print("i + z[i] activated")
+                center, right = i, i + Z[i]
+            print(Z[i])
+        return Z
+    res = manachers(S)
+    print(res)
+    return sum((v+1)/2 for v in res)
+
+arr = "abcaaaaac"
+countSubstrings(arr)
