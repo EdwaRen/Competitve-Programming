b75c39e6ef96c369a8bec667b63710f9ab55fdac
Eddie <eddie.ren.2013@gmail.com>
2019-02-26 22:37:27 -0700

    palindromic substrings O(n^2)

--- LC/647.palindromic-substrings.py
+++ LC/647.palindromic-substrings.py
@@ -1,11 +1,24 @@
 class Solution(object):
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
 
-def countSubstrings(S):
+    def countSubstrings(self, S):
+        res = 0
+
+        # Account for centers in between characters
+        for i in range((2*len(S))-1):
+            left = int(i / 2)
+            right = int((i+1)/2)
+
+            # Perform expand and match at each center
+            while left >= 0 and right < len(S):
+                if S[left] == S[right]:
+                    res +=1
+                else:
+                    left = -1
+                left -=1
+                right +=1
+
+        return res
+
     def manachers(S):
         A = '@#' + '#'.join(S) + '#$'
         print('#'.join(S))
@@ -26,9 +39,10 @@ def countSubstrings(S):
                 center, right = i, i + Z[i]
             print(Z[i])
         return Z
-    res = manachers(S)
-    print(res)
-    return sum((v+1)/2 for v in res)
+        res = manachers(S)
+        print(res)
+        return sum((v+1)/2 for v in res)
 
-arr = "abcaaaaac"
-countSubstrings(arr)
+z = Solution()
+arr = "fdsklf"
+print(z.countSubstrings(arr))
