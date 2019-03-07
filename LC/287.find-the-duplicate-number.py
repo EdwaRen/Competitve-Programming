5a1accad28c22fae851db004f08b4fa4c02c4c74
Eddie <eddie.ren.2013@gmail.com>
2019-03-06 23:25:09 -0700

    find duplicate number

--- LC/287.find-the-duplicate-number.py
+++ LC/287.find-the-duplicate-number.py
@@ -1,7 +1,26 @@
-wqclass Solution(object):
+class Solution(object):
     def findDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+        crawl = nums[0]
+        run = nums[0]
+        init = True
+        while crawl != run or init:
+            init = False
+            crawl = nums[crawl]
+            run = nums[nums[run]]
+
+#            print("crawl, run", crawl, run)
+
+        crawl = nums[0]
+        while crawl != run:
+            run = nums[run]
+            crawl = nums[crawl]       
+
+        return crawl
+
+z = Solution()
+arr = [3, 1, 3, 4, 2]
+print(z.findDuplicate(arr))
+
+
+
+ 
