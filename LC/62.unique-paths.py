8a689b8d42794ad21b0c366bed632241c7942eed
Edward Ren <edward.ren@ibm.com>
2019-03-09 13:40:25 -0700

    unique paths

--- LC/62.unique-paths.py
+++ LC/62.unique-paths.py
@@ -1,8 +1,25 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-        
+        arr = [1] * (m)
+        #arr[0] = 0
+        #print(arr)
+        for i in range(1, n):
+            prev = 0
+            for j in range(m):
+                arr[j] = arr[j] + prev
+                prev = arr[j]
+            #print(arr)
+        #print(arr)
+        return arr[-1]
+
+z = Solution()
+m = 7
+n = 3
+print(z.uniquePaths(m, n))   
+
+
+
+
+
+
+ 
