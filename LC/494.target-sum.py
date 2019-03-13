9895e7093c45e6d1dbe6a3703bd1a2a6bc63695e
Edward Ren <edward.ren@ibm.com>
2019-03-13 14:45:17 -0600

    target sum

--- LC/494.target-sum.py
+++ LC/494.target-sum.py
@@ -1,7 +1,18 @@
 class Solution:
     def findTargetSumWays(self, nums, S):
-        
+        p_sum = sum(nums) + s     
+        if p_sum & 1:
+            return 0
+        p_sum = p_sum >> 1
 
+        dp = [0] * (sum(nums) + 1)
+        
+        for i in range(1, len(nums)):
+            local_sum = 0
+            for j in reverse(i):
+                local_sum +=j
+                dp[local_sum] +=1 
+                
 
 
 
