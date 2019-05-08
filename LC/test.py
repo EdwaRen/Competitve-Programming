83a9ae4db224ae636ed6ffc079fd80facd3e75fa
Eddie <eddie.ren.2013@gmail.com>
2019-05-08 13:46:08 -0400

    updated test and random files

--- LC/test.py
+++ LC/test.py
@@ -1,3 +1,15 @@
 s1 = "/3 /4 /1 # #  /2 # #   /5 # #  "
 s2 = "2"
 print(s2 in s1)
+
+s = "cababb"
+c = min((s),key=s.count)
+print(c)
+
+print(s.count(c))
+
+print("division test",int( 3/2))
+
+arr = [1, 2, 3, 4]
+print(arr[5:4])
+
