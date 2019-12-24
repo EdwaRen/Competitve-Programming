c91c89df0ee382e36557a9009fa6f05a68523290
Eddie <eddie.ren.2013@gmail.com>
2019-12-23 22:10:13 -0500

    Better docs for Pqueues

--- Code Snippets/Priority_Queue.py
+++ Code Snippets/Priority_Queue.py
@@ -7,9 +7,11 @@
 import queue as Q
 q = Q.PriorityQueue()
 
+# Smaller keys get popped out first
 q.put((3, 5))
 q.put((4, 2))
+q.put((1, 2))
 
-print("test")
+print("test should return (1, 2)")
 print(q.get()) # there is not q.peak with this implementation
 print(q.empty())
