02d49709b978b7681ecb7c4fcce0022afcf99ce1
Eddie <eddie.ren.2013@gmail.com>
2020-01-01 22:46:14 -0500

    clean code snippets

--- Code Snippets/Priority_Queue.py
+++ Code Snippets/Priority_Queue.py
@@ -15,3 +15,15 @@ q.put((1, 2))
 print("test should return (1, 2)")
 print(q.get()) # there is not q.peak with this implementation
 print(q.empty())
+
+print("Heapq implementation")
+import heapq
+nums = [4, 5, 1, 2, 7, 8, 3]
+heapq.heapify(nums)
+heapq.heappush(nums, 9)
+print("return smallest:", 0)
+print(heapq.heappushpop(nums, 0))
+print("return 2nd smallest:", 1)
+print(heapq.heappushpop(nums, 9))
+
+
