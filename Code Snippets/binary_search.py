f38cecc85122cbba755c08cc5a9a90a66a6569ac
Eddie <edward.ren@shopify.com>
2021-01-18 22:28:51 -0500

    Added quickselect to Code snippets, updated binary search

--- Code Snippets/binary_search.py
+++ Code Snippets/binary_search.py
@@ -23,3 +23,33 @@ print("Finds 9, expected True")
 print(binary_search(nums, 9))
 print("Finds 0, expected False")
 print(binary_search(nums, 0))
+
+# Alternative implementation with custom conditions
+# Plagiarized from here: https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/discuss/777019/Python-Clear-explanation-Powerful-Ultimate-Binary-Search-Template.-Solved-many-problems.
+def smallestDivisor(nums, threshold):
+    def condition(divisor):
+        return sum((num - 1) // divisor + 1 for num in nums) <= threshold
+
+    left, right = 1, max(nums)
+    while left < right:
+        mid = left + (right - left) // 2
+        if condition(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+print("\n\nAlternative binary search to find a condition instead of \
+finding a number:")
+
+nums = [44,22,33,11,1]
+T = 5
+print(smallestDivisor(nums, T))
+
+nums = [21212,10101,12121]
+T = 1000000
+print(smallestDivisor(nums, T))
+
+nums = [2,3,5,7,11]
+T = 11
+print(smallestDivisor(nums, T))
