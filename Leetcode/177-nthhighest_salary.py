426f07c3c80fbdfddb4d1ac1f8be94a418f35102
Eddie <edward.ren@shopify.com>
2021-02-01 21:21:04 -0500

    nth highest salary

---
+++ Leetcode/177-nthhighest_salary.py
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select distinct Salary
+      from Employee
+      order by Salary desc
+      LIMIT M, 1
+  );
+END
