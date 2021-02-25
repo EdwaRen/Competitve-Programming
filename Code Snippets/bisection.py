13ba098d19289d3167dc5db8a3deeb7170677d91
Eddie <eddie.ren.2013@gmail.com>
2021-02-24 23:52:10 -0500

    code snippets

--- Code Snippets/bisection.py
+++ Code Snippets/bisection.py
@@ -9,3 +9,9 @@ print("bisect right for key = 3: ", idx_right)
 idx_left = bisect.bisect_left(keys, 3)
 print("bisect left for key = 3: ", idx_left)
 
+idx_left = bisect.bisect_left(keys, 4)
+print("bisect left for key = 4: ", idx_left)
+
+idx_right = bisect.bisect_right(keys, 4)
+print("bisect right for key = 4: ", idx_right)
+
---
+++ Code Snippets/gen_10000.py
@@ -0,0 +1,2 @@
+for i in reversed(range(10000)):
+    print(i, end=' ')
