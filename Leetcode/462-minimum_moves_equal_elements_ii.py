c4508765cfd05519e213a62b388889d141f197c9
Eddie <edward.ren@shopify.com>
2021-04-28 21:29:08 -0400

    minimum moves equal elements

---
+++ Leetcode/462-minimum_moves_equal_elements_ii.py
@@ -0,0 +1,41 @@
+import random
+
+class Solution(object):
+    def minMoves2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        def partition(data, pivot):
+            less, equal, greater = [], [], []
+            for item in data:
+                if item < pivot:
+                    less.append(item)
+                elif item > pivot:
+                    greater.append(item)
+                else:
+                    equal.append(item)
+            return less, equal, greater
+
+
+        def quickselect(nums, index):
+            pivot = nums[random.randint(0, len(nums) - 1)]
+            count = 0
+            smaller, equal, larger = partition(nums, pivot)
+            count = len(equal)
+            mid = len(smaller)
+
+            if mid <= index and index < mid + count:
+                return pivot
+            elif mid > index:
+                return quickselect(smaller, index)
+            else:
+                return quickselect(larger, index - (mid + count))
+
+        median = quickselect(nums, (len(nums)//2))
+        return sum(abs(median-i) for i in nums)
+
+z = Solution()
+nums = [1, 3, 2]
+print(z.minMoves2(nums))
